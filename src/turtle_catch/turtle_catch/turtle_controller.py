54fd8e07286e28d85f81d53ba242a5bd60a225a7
fog <miwusenlin.g@qq.com>
2026-05-14 23:33:07 +0800

    实现turtle_catch项目

--- src/turtle_catch/turtle_catch/turtle_controller.py
+++ src/turtle_catch/turtle_catch/turtle_controller.py
@@ -2,32 +2,60 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
+from base_interface.msg import Turtle, TurtleArray
+from base_interface.srv import CatchTurtle
+from functools import partial
 import math
                              
 class TurtleControllerNode(Node):
     def __init__(self):
         super().__init__('turtle_controller')
+        self.declare_parameter("catch_closest_turtle_first", True)
+        self.catch_closest_turtle_first_=self.get_parameter("catch_closest_turtle_first").value
+
         self.pose_=None
-        self.targle_x=9.5
-        self.targle_y=1.0
-        self.cmd_pub_=self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
-        self.pose_sub_=self.create_subscription(Pose, '/turtle1/pose', self.pose_callback, 10)
-        self.control_loop_timer_ = self.create_timer(0.02, self.control_loop)  # 50 Hz 控制循环
+        self.turtle_to_catch_=None
+        self.cmd_pub_=self.create_publisher(
+            Twist, '/turtle1/cmd_vel', 10)
+        self.pose_sub_=self.create_subscription(
+            Pose, '/turtle1/pose', self.pose_callback, 10)
+        self.alive_turtles_sub_=self.create_subscription(
+            TurtleArray, 'alive_turtles', self.alive_turtles_callback, 10)
+        self.catch_turtle_client_=self.create_client(CatchTurtle, 'catch_turtle')
+        self.control_loop_timer_ = self.create_timer(
+            0.02, self.control_loop)  # 50 Hz 控制循环
     # pose_callback：接收乌龟的位姿信息，更新 self.pose_ 以供控制循环使用
     def pose_callback(self, pose: Pose):
         self.pose_ = pose
-       
+    # alive_turtles_callback：接收存活乌龟列表，更新 self.alive_turtles_ 以供控制循环使用
+    def alive_turtles_callback(self, turtles: TurtleArray):
+        if len(turtles.turtles)>0:
+            if self.catch_closest_turtle_first_:
+                closest_turtle=None
+                min_turtle_distance=float('inf')
+                # 找到最近的乌龟
+                for turtle in turtles.turtles:
+                    distance=(turtle.x-self.pose_.x)**2+(turtle.y-self.pose_.y)**2
+                    if distance<min_turtle_distance or closest_turtle == None:
+                        min_turtle_distance=distance
+                        closest_turtle=turtle
+                self.turtle_to_catch_ = closest_turtle
+            else:
+                self.turtle_to_catch_ = turtles.turtles[0]
+    # control_loop：控制循环函数，根据当前位姿和目标点计算控制指令，发布到 /turtle1/cmd_vel 话题
     def control_loop(self):
-        if self.pose_ ==None:
+        if self.pose_ ==None or self.turtle_to_catch_ is None:
             return  # 尚未收到位姿信息，等待下一次循环
-        dist_x=self.targle_x - self.pose_.x
-        dist_y=self.targle_y - self.pose_.y
+        dist_x=self.turtle_to_catch_.x - self.pose_.x
+        dist_y=self.turtle_to_catch_.y - self.pose_.y
         dist=math.sqrt(pow(dist_x,2)+pow(dist_y,2))
         cmd = Twist()
         if dist<0.5:
             self.get_logger().info('目标点已到达！')
             cmd.linear.x = 0.0
             cmd.angular.z = 0.0
+            self.catch_turtle_service(self.turtle_to_catch_.name)
+            self.turtle_to_catch_=None
         else:
             cmd.linear.x = 2*dist  # 前进速度
             theta=math.atan2(dist_y,dist_x)-self.pose_.theta
@@ -39,6 +67,23 @@ class TurtleControllerNode(Node):
 
         # 发布控制指令
         self.cmd_pub_.publish(cmd)
+    #调用catch_turtle 服务
+    def catch_turtle_service(self,turtle_name):
+        while not self.catch_turtle_client_.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('等待 catch_turtle 服务...')
+            return
+        req=CatchTurtle.Request()
+        req.name=turtle_name
+        future=self.catch_turtle_client_.call_async(req)
+        future.add_done_callback(partial(
+            self.callback_catch_service,turtle_name=turtle_name))
+    #catch_turtle回调函数
+    def callback_catch_service(self,future,turtle_name):
+        response=future.result()
+        if response.success:
+            self.get_logger().info(f'成功捕获乌龟{turtle_name}！')
+        else:
+            self.get_logger().error(f'捕获乌龟{turtle_name}失败！')
 
 def main():
     rclpy.init()
--- src/turtle_catch/turtle_catch/turtle_spawner.py
+++ src/turtle_catch/turtle_catch/turtle_spawner.py
@@ -4,22 +4,47 @@ from functools import partial
 import random
 from turtlesim.srv import Spawn
 from base_interface.msg import Turtle, TurtleArray
+from base_interface.srv import CatchTurtle
+from turtlesim.srv import Kill
                              
 class TurtleSpawnerNode(Node):
     def __init__(self):
         super().__init__('turtle_spawner')
-        self.turtle_name_prefix_ = "t"
+
+        self.declare_parameter('turtle_name_prefix', 't')
+        self.turtle_name_prefix_ = self.get_parameter('turtle_name_prefix').value
+        self.declare_parameter("turtle_frequency", 0.8)  # 每秒生成的乌龟数量
+        self.turtle_frequency_ = self.get_parameter("turtle_frequency").value
+
         self.turtle_count_ = 0
         self.alive_turtles_=[]
         self.alive_turtles_pub_ = self.create_publisher(TurtleArray, 'alive_turtles', 10)
         self.spawn_client_ = self.create_client(Spawn, '/spawn')
-        self.spawn_turtle_timer_ = self.create_timer(1.0, self.spawn_turtle_timer_callback)  # 每隔 1 秒尝试调用一次 spawn 服务
+        self.kill_client_ = self.create_client(Kill, '/kill')
+
+        self.catch_turtle_service_ = self.create_service(
+            CatchTurtle, 'catch_turtle', self.catch_turtle_callback)
+        self.spawn_turtle_timer_ = self.create_timer(
+            self.turtle_frequency_, self.spawn_turtle_timer_callback)  # 每隔 1 秒尝试调用一次 spawn 服务
+    # catch_turtle_callback：捕获乌龟的服务回调函数，接收要捕获的乌龟名字，调用 /kill 服务删除该乌龟，并在响应中返回捕获结果
+    def catch_turtle_callback(self, request, response):
+        turtle_name=request.name
+        # 调用 /kill 服务删除被捕获的乌龟
+        while not self.kill_client_.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('等待 /kill 服务...')
+        kill_req=Kill.Request()
+        kill_req.name=turtle_name
+        future=self.kill_client_.call_async(kill_req)
+        future.add_done_callback(partial(self.callback_call_kill_service,turtle_name=turtle_name))
+        response.success=True
+        return response
+
     # spawn_new_turtle：生成新的乌龟，名字为 t1、t2、t3...，位置随机分布在 turtlesim 的 11x11 范围内，朝向随机
     def spawn_new_turtle(self):
         self.turtle_count_ += 1
         name = f"{self.turtle_name_prefix_}{self.turtle_count_}"
-        x = random.uniform(0.1, 11.0)
-        y = random.uniform(0.1, 11.0)
+        x = random.uniform(0.1, 10.5)
+        y = random.uniform(0.1, 10.5)
         theta = random.uniform(0.0, 6.28)
         self.call_spawn_service(x, y,theta, name)
     # pub_alive_turtles：发布当前存活的乌龟列表，消息类型为 base_interface/msg/TurtleArray
@@ -43,7 +68,8 @@ class TurtleSpawnerNode(Node):
         req.name = name
         future = self.spawn_client_.call_async(req)
         future.add_done_callback(partial(self.callback_call_spawn_service,request=req))
-
+    #生成乌龟的回调函数：接收 /spawn 服务的响应
+    #判断是否成功生成乌龟，如果成功则将新乌龟添加到 alive_turtles_ 列表并发布更新后的列表
     def callback_call_spawn_service(self, future,request):
         response=future.result()
         if response.name != "":
@@ -51,13 +77,20 @@ class TurtleSpawnerNode(Node):
             # 将新生成的乌龟添加到 alive_turtles_pub_ 发布的消息中
             turtle = Turtle()
             turtle.name = response.name
-            turtle.x = response.x
-            turtle.y = response.y
-            turtle.theta = response.theta
+            turtle.x = request.x
+            turtle.y = request.y
+            turtle.theta = request.theta
             self.alive_turtles_.append(turtle) 
             self.pub_alive_turtles() # 发布更新后的乌龟列表
         else:
             self.get_logger().error('调用 /spawn 服务失败')
+    # callback_call_kill_service：调用 /kill 服务的回调函数，参数为被捕获乌龟的名字
+    def callback_call_kill_service(self, future, turtle_name):
+        for(i,turtle) in enumerate(self.alive_turtles_):
+            if turtle.name==turtle_name:
+                del self.alive_turtles_[i] 
+                self.pub_alive_turtles()
+                break
 
 def main():
     rclpy.init()
